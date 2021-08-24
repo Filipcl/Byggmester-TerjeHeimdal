ccefb5535f1b5089c57f1dfc1298336e5f3a980e
filipcl <filip.larsen@live.no>
2021-08-24 20:20:19 +0200

    added static display

--- index.js
+++ index.js
@@ -52,13 +52,13 @@ app.post("/api/form", (req, res) => {
   });
 });
 
-/* if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-} */
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
